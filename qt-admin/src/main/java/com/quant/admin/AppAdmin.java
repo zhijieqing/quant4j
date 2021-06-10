f84424d2b1c69deb9fe3e98a53466aa24de581fc
zhijie.he <zhijie.he@ucarinc.com>
2021-06-10 17:56:05 +0800

    local

--- qt-admin/src/main/java/com/quant/admin/AppAdmin.java
+++ qt-admin/src/main/java/com/quant/admin/AppAdmin.java
@@ -2,13 +2,13 @@ package com.quant.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 @EnableScheduling
 @ComponentScan({"com.quant.common", "com.quant.admin"})
 public class AppAdmin {
